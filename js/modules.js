1921020db523bd0f83f5dcc6f349e05ea0a8872a
Esteban Selle <esteban@MacBook-Pro.local>
2015-11-01 16:37:48 -0300

    implemented products rendered based on data from server

--- js/modules.js
+++ js/modules.js
@@ -63,23 +63,21 @@ CORE.create_module("filters-bar", function (sb) {
 CORE.create_module("product-panel", function (sb) {
     var products;
 
-    function eachProduct(fn) {
+    function each(array, fn) {
         var i = 0, product;
-        for ( ; product = products[i++]; ) {
+        for ( ; product = array[i++]; ) {
             fn(product);
         }
     }
     
     function reset () {
-        eachProduct(function (product) {
+        each(products, function (product) {
             product.style.opacity = '1';
         });
     }
 
     return {
         init : function () {
-            var that = this;
-
             this.getProducts();
 
             sb.listen({
@@ -88,40 +86,54 @@ CORE.create_module("product-panel", function (sb) {
                 'perform-search': this.search,
                 'quit-search'   : this.reset
             });
-            eachProduct(function (product) {
-                sb.addEvent(product, 'click', that.addToCart);
-            });
         },
         destroy : function () {
             var that = this;
-            eachProduct(function (product) {
+            each(products, function (product) {
                 sb.removeEvent(product, 'click', that.addToCart);
             });
             sb.ignore(['change-filter', 'reset-filter', 'perform-search', 'quit-search']);
         },
         getProducts: function () {
             var serverProducts;
+            var that = this;
             
             sb.login('moravia', 'argentina', function(response) {
                 
                 sb.getData(function (response) {
                     if(response.length) {
-                        serverProducts = response;
+                        that.createProducts(response);
                     };
                 })
             });
-            
-            
-            products = sb.find("li");
-            
         },
-        createProducts: function (serverProducts) {
-            console.log(serverProducts);
+        createProducts: function (productsResponse) {
+            var productsContainer = sb.find('ul')[0];
+            var that = this;
+            
+            each(productsResponse, function (product) {
+                var productItem = sb.create_element(
+                    'li',
+                    { 
+                        id : product.id,
+                        'data-8088-keyword': product.keyword,                                 
+                        children : [ 
+                            sb.create_element('img', { 'src' : product.file }), 
+                            sb.create_element('p', { text : product.name })
+                        ]
+                    }
+                );
+                
+                sb.addEvent(productItem, 'click', that.addToCart);
+                productsContainer.appendChild(productItem);
+            });
+            
+            products = sb.find('li'); 
         },
         reset : reset,
         change_filter : function (filter) {
             reset();
-            eachProduct(function (product) {
+            each(products, function (product) {
                 if (product.getAttribute("data-8088-keyword").toLowerCase().indexOf(filter.toLowerCase()) < 0) {
                     product.style.opacity = '0.2';
                 }
@@ -130,11 +142,54 @@ CORE.create_module("product-panel", function (sb) {
         search : function (query) {
             reset();
             query = query.toLowerCase();
-            eachProduct(function (product) {
+            each(products, function (product) {
                 if (product.getElementsByTagName('p')[0].innerHTML.toLowerCase().indexOf(query) < 0) {
                     product.style.opacity = '0.2';
                 }
             });
+        },
+        addToCart : function (e) {
+            var li = e.currentTarget;
+        
+            sb.notify({
+                type: 'add-to-cart',
+                data: {
+                    id: li.id,
+                    name: li.getElementsByTagName('p')[0].innerHTML
+                }
+            })
+        }
+    };
+});
+
+CORE.create_module("shopping-cart", function (sb) {
+    var cart;
+    
+    return {
+        init: function () {
+            cart = sb.find("#shopping-list")[0];
+            
+            sb.listen({
+                'add-to-cart': this.addProduct
+            })
+        },
+        
+        //Here I'm doing the same as in product-panel, erasing objects and removing events
+        destroy: function () {
+            cart = null;
+            
+            sb.ignore(['add-to-cart']);
+        },
+        
+        addProduct : function (product) { 
+            var cartItem = sb.find('#cart-item' + product.id)[0]; 
+            if (!cartItem) { 
+                cartItem = sb.create_element('li', { id : "cart-item" + product.id, children : [ 
+                        sb.create_element('span', { 'class' : 'product_name', text : product.name }), 
+                        ]}); 
+
+                cart.appendChild(cartItem);
+            }
         }
     };
 });
